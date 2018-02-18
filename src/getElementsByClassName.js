de83032dfea47ab5df48982bc4bf5bd5528918f6
Mo Lee <2000prcs@gmail.com>
2018-02-17 23:07:54 -0800

    Implement getElementsByClassName (complete Recursion)
    getElementsByClassName should work same as document.getElementsByClassName.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+
+var getElementsByClassName = function (className, element = document.body) {
   // your code here
+  var targetClasses = [];
+
+  var childNodes = element.childNodes;
+
+  if (element.classList && element.classList.contains(className)) {
+    targetClasses.push(element);
+  }
+
+  for (var i = 0; i < childNodes.length; i++) {
+    var inner = getElementsByClassName(className, childNodes[i]);
+    targetClasses = targetClasses.concat(inner);
+
+  }
+  return targetClasses;
 };
